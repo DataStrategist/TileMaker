e903d3e89e71e918fa308b63dcab93ed996e182b
Amit <mexindian@gmail.com>
2016-02-17 18:22:25 +0000

    Added optional units, and possibility to change tile colors automatically based on value and thresholds.

--- tilemaker.R
+++ tilemaker.R
@@ -30,33 +30,47 @@ DivMaker <- function(Title="",Buttons){
 }
 
 
-ButtonMaker <- function(Color=1,Size=4,Value,Subtitle="",Link="",Icon=""){
+ButtonMaker <- function(Color=1,Size=4,Value,Subtitle="",Link="",Icon="", Units="",
+                        Target=0,ThresholdHigh=0,ThresholdLow=0){
   ## colors
-  colorList = c("default",  "primary",  "success",  "info",  "warning",  "danger")
+  colorList = c("success",  "warning", "danger", "info", "primary", "default")
     
   ## sizes:
   SizeList = c("xs","sm","md","lg")
   
   ## for icons, goto http://getbootstrap.com/components/
   
-  paste('<',
+  paste(paste('<',
         if(Link !=""){paste('a href="',Link,'" role="button" ',sep='')} else{'button'},
-        ' type="button" class="btn btn-',
-        colorList[Color],' btn-', SizeList[Size],'"><h1>',
+        ' type="button" class="btn ',sep=''),
+        if(Target ==0){
+          paste('btn-',colorList[Color],sep='')
+          } else {
+            Perc <- Value/Target *100
+            if(Perc > ThresholdHigh){
+              'btn-success'
+            } else if(Perc< ThresholdLow){
+              'btn-danger'
+            } else {
+              'btn-warning'
+            }
+            },
+  paste(' btn-', SizeList[Size],'"><h1>',sep=''),
         if(Icon !=""){paste(' <span class="',Icon,'" aria-hidden="true"></span> ',sep='')},
-        Value,
+        if(Units == ""){Value} else {paste(Value,Units,sep="")},
         '</h1>',
         Subtitle,
         if(Link !=""){'</a>'} else{'</button>'},
         sep="")
 }
 
-
-
 #########################################
 
-Button1 <- ButtonMaker(Color = 2,Value = 3.57,Subtitle = "Times apple eaten",Link = "https://en.wikipedia.org/wiki/Apple",Icon="glyphicon glyphicon-apple")
-Button2 <- ButtonMaker(Color = 3,Value = 13.7,Subtitle = "Nutritional value")
+Button1 <- ButtonMaker(Color = 2,Value = 65,Subtitle = "Times apple eaten",
+                       Link = "https://en.wikipedia.org/wiki/Apple",Icon="glyphicon glyphicon-apple", 
+                       Target=100,ThresholdHigh=70,ThresholdLow=40)
+Button2 <- ButtonMaker(Color = 3,Value = 93.7,Subtitle = "Nutritional value", 
+                       Target=100,ThresholdHigh=70,ThresholdLow=40,Units="%")
 Button3 <- ButtonMaker(Color = 4,Value = 1,Subtitle = "Yumminess factor")
 Button4 <- ButtonMaker(Color = 5,Size=1,Value = 5,Subtitle = "Inconsistencies",Icon = "glyphicon glyphicon-pushpin")
 
