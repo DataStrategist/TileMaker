66a5b04011da1f0a10934dbaa7a63f4957e686e0
Joshua <jbkunst@gmail.com>
2017-06-06 15:13:46 -0400

    adding tidyverse-styled-tile-marker

---
+++ R/tile_maker.R
@@ -0,0 +1,69 @@
+library(htmltools)
+
+ico <- function(x) {
+
+  if(is.null(x)) NULL else tags$i(class = "glyphicon", class = paste0("glyphicon-", x))
+
+}
+
+button_maker <- function(value = NULL, subtitle = NULL, size = "md", icon = NULL,
+                         type = "warning", link = NULL, units = NULL, hover = NULL) {
+
+  tags$a(
+    href = link,
+    title = hover,
+    type = "button", role = "button",
+    # classes: size, color
+    class = "btn", class = paste0("btn-", size), class = paste0("btn-", type),
+    tags$h1(ico(icon), value, units),
+    subtitle
+  )
+
+}
+
+div_maker <- function(title = NULL, ...) {
+
+  tags$div(
+    class = "container",
+    tags$h2(title),
+    ...
+  )
+
+}
+
+tile_maker <- function(title = NULL, ..., css = "https://bootswatch.com/flatly/bootstrap.css") {
+
+  tl <- tags$html(
+    tags$head(
+      tags$link(rel = "stylesheet", href = css)
+    ),
+    tags$body(
+      tags$h1(title),
+      ...
+    )
+  )
+
+  browsable(tl)
+
+}
+
+ico(NULL)
+ico("apple")
+
+b1 <- button_maker(value = 3.57, subtitle = "Times apple eaten", icon = "apple")
+b2 <- button_maker(value = 13.7, subtitle = "Nutritional value", size = "lg")
+b3 <- button_maker(value = 1, subtitle = "Yumminess factor", type = "danger")
+b4 <- button_maker(value = 5, subtitle = "Inconsistencies", hover = "This is the description")
+
+d1 <- div_maker(title = "Quantativity factors", b1, b2)
+d2 <- div_maker(title = "Implementation procedures", b3, b4)
+
+tile_maker(title = "Hello", d1, d2)
+
+map <- leaflet::addTiles(leaflet::leaflet())
+map
+
+tile_maker(title = "Hello", d1, map, d2)
+
+
+
